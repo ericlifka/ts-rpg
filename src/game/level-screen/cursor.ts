92853825c72a9c6ad4e3d67c850b99afee8a3a8e
Eric Lifka <eric.lifka@inin.com>
2017-04-15 23:12:42 -0400

    move input logic up to level manager because of conflicts with checking bounds

--- src/game/level-screen/cursor.ts
+++ src/game/level-screen/cursor.ts
@@ -12,57 +12,26 @@ export default class Cursor extends GameEntity {
   center: Coordinate;
   position: Coordinate;
   tileSize: number;
-  movementClear: number = 0;
+  halfTileSize: number;
 
   constructor(parent, public camera: Camera, public gridPosition: Coordinate) {
     super(parent);
 
     this.sprite = createCursorSprite();
     this.tileSize = this.sprite.dimensions.width;
+    this.halfTileSize = Math.floor(this.tileSize / 2);
 
     this.calculatePositionCoordinates();
-  }
-
-  update(dtime: number, inputs: any[]): void {
-    super.update(dtime, inputs);
-
-    this.movementClear += dtime;
-    if (this.movementClear > 0) {
-      inputs.forEach(input => {
-        if (input.INPUT_TYPE === "keyboard") {
-
-          let direction: Coordinate = {x: 0, y: 0};
-          if (input.W) {
-            console.log("UP!");
-            direction.y += 1;
-          }
-          if (input.A) {
-            console.log("LEFT!");
-            direction.x -= 1;
-          }
-          if (input.S) {
-            console.log("DOWN!");
-            direction.y -= 1;
-          }
-          if (input.D) {
-            console.log("RIGHT!");
-            direction.x += 1;
-          }
-
-          if (direction.x !== 0 || direction.y !== 0) {
-            this.movementClear = -500;
-            this.gridPosition.x += direction.x;
-            this.gridPosition.y += direction.y;
-            this.calculatePositionCoordinates();
-          }
-        }
-      });
-    }
   }
 
   render(frame: CellGrid): void {
     let screenCoord = this.camera.mapToScreenCoord(this.position);
     this.sprite.render(frame, screenCoord, OVERLAY);
+  }
+
+  moveTo(gridPosition: Coordinate): void {
+    this.gridPosition = gridPosition;
+    this.calculatePositionCoordinates();
   }
 
   calculatePositionCoordinates() {
@@ -71,8 +40,8 @@ export default class Cursor extends GameEntity {
       y: this.gridPosition.y * this.tileSize
     };
     this.position = {
-      x: this.center.x - Math.floor(this.tileSize / 2),
-      y: this.center.y - Math.floor(this.tileSize / 2)
+      x: this.center.x - this.halfTileSize,
+      y: this.center.y - this.halfTileSize
     };
   }
 }
--- src/game/level-screen/level-manager.ts
+++ src/game/level-screen/level-manager.ts
@@ -2,15 +2,17 @@ import GameEntity from "../../pxlr/core/game-entity";
 import CellGrid from "../../pxlr/core/cell-grid";
 import Camera from "./camera";
 import {createGrassSprite} from "../sprites/tiles/grass";
-import {Dimension} from "../../pxlr/utils/types";
+import {Coordinate, Dimension} from "../../pxlr/utils/types";
 import LevelTile from "./level-tile";
 import Cursor from "./cursor";
+import {clamp} from "../../pxlr/utils/clamp";
 
 export default class LevelManager extends GameEntity {
 
   camera: Camera;
   cursor: Cursor;
   levelGrid: LevelTile[][];
+  movementClear: number = 0;
 
   constructor(parent, public dimensions: Dimension) {
     super(parent);
@@ -37,6 +39,37 @@ export default class LevelManager extends GameEntity {
 
   update(dtime: number, inputs: any[]): void {
     super.update(dtime, inputs);
+
+    this.movementClear += dtime;
+    if (this.movementClear > 0) {
+      inputs.forEach(input => {
+        if (input.INPUT_TYPE === "keyboard") {
+
+          let direction: Coordinate = {x: 0, y: 0};
+          if (input.W) {
+            direction.y += 1;
+          }
+          if (input.A) {
+            direction.x -= 1;
+          }
+          if (input.S) {
+            direction.y -= 1;
+          }
+          if (input.D) {
+            direction.x += 1;
+          }
+
+          let newPosition = {
+            x: clamp(this.cursor.gridPosition.x + direction.x, 0, 2),
+            y: clamp(this.cursor.gridPosition.y + direction.y, 0, 2)
+          };
+          if (newPosition.x !== this.cursor.gridPosition.x || newPosition.y !== this.cursor.gridPosition.y) {
+            this.movementClear = -500;
+            this.cursor.moveTo(newPosition);
+          }
+        }
+      });
+    }
   }
 
   render(frame: CellGrid): void {
