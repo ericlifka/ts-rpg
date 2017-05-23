cd9b53bf95bb7be0ee2f62e60671118d4f394329
Eric Lifka <eric.lifka@inin.com>
2017-05-22 23:32:29 -0400

    offset the sprite so it's mostly centered

--- src/game/level-screen/entities/character.ts
+++ src/game/level-screen/entities/character.ts
@@ -3,18 +3,20 @@ import Sprite from "../../../pxlr/core/sprite";
 import LevelTile from "../level-tile";
 import Camera from "../camera";
 import CellGrid from "../../../pxlr/core/cell-grid";
-import {Color, Coordinate, copyCoord} from "../../../pxlr/utils/types";
+import {addCoords, Color, Coordinate, copyCoord} from "../../../pxlr/utils/types";
 import {CHARACTER} from "../../../pxlr/utils/layers";
 
 export default class Character extends GameEntity {
   position: Coordinate;
+  displayOffset: Coordinate;
 
   constructor(parent, public camera: Camera, public sprite: Sprite) {
     super(parent);
+    this.displayOffset = { x: 10, y: 7 };
   }
 
   moveToTile(tile: LevelTile) {
-    this.position = copyCoord(tile.position);
+    this.position = addCoords(tile.position, this.displayOffset);
   }
 
   render(frame: CellGrid<Color>): void {
--- src/pxlr/utils/types.ts
+++ src/pxlr/utils/types.ts
@@ -24,6 +24,12 @@ export interface Color {
 export function copyCoord(target: Coordinate): Coordinate {
   return { x: target.x, y: target.y };
 }
+export function addCoords(left: Coordinate, right: Coordinate): Coordinate {
+  return {
+    x: left.x + right.x,
+    y: left.y + right.y
+  };
+}
 export function copyDimension(target: Dimension): Dimension {
   return { width: target.width, height: target.height };
 }
